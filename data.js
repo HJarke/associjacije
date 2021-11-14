246366ec0dbc13ee8603ba14a9d69794c836e1f3
HJarke <94316036+HJarke@users.noreply.github.com>
2021-11-14 19:43:58 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,24 @@
+function postavi() {
+    btn1.setAttribute("value", "a") //Vrednost polja A1
+    btn2.setAttribute("value", "aa") //Vrednost polja A2
+    btn3.setAttribute("value", "aaa") //Vrednost polja A3
+    btn4.setAttribute("value", "aaaa") //Vrednost polja A4
+    btn5.setAttribute("value", "b") //Vrednost polja B1
+    btn6.setAttribute("value", "bb") //Vrednost polja B2
+    btn7.setAttribute("value", "bbb") //Vrednost polja B3
+    btn8.setAttribute("value", "bbbb") //Vrednost polja B4
+    btn9.setAttribute("value", "c") //Vrednost polja C1
+    btn10.setAttribute("value", "cc") //Vrednost polja C2
+    btn11.setAttribute("value", "ccc") //Vrednost polja C3
+    btn12.setAttribute("value", "cccc") //Vrednost polja C4
+    btn13.setAttribute("value", "d") //Vrednost polja D1
+    btn14.setAttribute("value", "dd") //Vrednost polja D2
+    btn15.setAttribute("value", "ddd") //Vrednost polja D3
+    btn16.setAttribute("value", "dddd") //Vrednost polja D4
+    a.setAttribute("value", "qa") //Resenje kolone A
+    b.setAttribute("value", "qb") //Resenje kolone B
+    c.setAttribute("value", "qc") //Resenje kolone C
+    d.setAttribute("value", "qd") //Resenje kolone D
+    kon.setAttribute("value", "kon") //Konacno resenje
+    }
+postavi()
---
+++ java.js
@@ -0,0 +1,55 @@
+import("./data.js")
+
+const btn1 = document.querySelector("#fBtn1");
+const btn2 = document.querySelector("#fBtn5");
+const btn3 = document.querySelector("#fBtn9");
+const btn4 = document.querySelector("#fBtn13");
+const btn5 = document.querySelector("#fBtn2");
+const btn6 = document.querySelector("#fBtn6");
+const btn7 = document.querySelector("#fBtn10");
+const btn8 = document.querySelector("#fBtn14");
+const btn9 = document.querySelector("#fBtn3");
+const btn10 = document.querySelector("#fBtn7");
+const btn11 = document.querySelector("#fBtn11");
+const btn12 = document.querySelector("#fBtn15");
+const btn13 = document.querySelector("#fBtn4");
+const btn14 = document.querySelector("#fBtn8");
+const btn15 = document.querySelector("#fBtn12");
+const btn16 = document.querySelector("#fBtn16");
+const a = document.querySelector("#fBtn17");
+const b = document.querySelector("#fBtn18");
+const c = document.querySelector("#fBtn19");
+const d = document.querySelector("#fBtn20");
+const kon = document.querySelector("#eBtn");
+const pa = document.querySelector("#pA");
+const pb = document.querySelector("#pB");
+const pc = document.querySelector("#pC");
+const pd = document.querySelector("#pD");
+const pk = document.querySelector("#pK");
+
+
+
+document.querySelectorAll(".fBtn").forEach(item => {
+    item.addEventListener("click", event => {
+        item.innerHTML = item.getAttribute("value")
+    }) 
+});
+
+document.querySelectorAll(".sBtn").forEach(item => {
+    item.addEventListener("click", event => {
+        let res = prompt("Unesite resenje", "")
+        if(res == item.getAttribute("value")) {
+            item.innerHTML = "RES"
+        }
+        else {
+            alert("Neispravno!")
+            switch(item.getAttribute("value")) {
+                case a.getAttribute("value"): pa.innerHTML += res + "; "; break;
+                case b.getAttribute("value"): pb.innerHTML += res + "; "; break;
+                case c.getAttribute("value"): pc.innerHTML += res + "; "; break;
+                case d.getAttribute("value"): pd.innerHTML += res + "; "; break;
+                default: pk.innerHTML +=  res + "; "; break;
+            }
+        }
+    }) 
+});
